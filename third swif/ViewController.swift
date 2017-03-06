cc2a2b7b82d7fd7176b24ba56b430933f326cb4b
ciec20151104696 <s20151104696@mac15.local>
2017-03-06 11:20:44 +0800

    Initial Commit

---
+++ third swif/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  third swif
+//
+//  Created by s20151104696 on 17/3/6.
+//  Copyright © 2017年 s20151104696. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
